b39b4d29393e8a87f2558fc8ca369d988d7072d8
Marko Mavrinac <mavrinac07@gmail.com>
2019-11-12 15:33:32 +0100

    removed deathstar base speed bonus

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import sys
 from cx_Freeze import setup, Executable
 
-includeFiles = ['preset.ini']
+includeFiles = ['preset.ini','changelog.txt']
 bdist_msi_options = {
     'add_to_path': False,
     'initial_target_dir': r'[ProgramFilesFolder]\LanxCalc',
     }
 setup(
     name = "LanxCalc",
-    version = "0.9b",
+    version = "0.9.2",
     description = "Calculate return times like a boss.",
     executables = [Executable("GUI.py", base = "Win32GUI", icon = "icon.ico")],
     options={'bdist_msi': bdist_msi_options,'build_exe':{'include_files':includeFiles}})
